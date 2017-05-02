658f2440ae188ac71b0a746cf6a9fa2b29c847ea
aknuth <andreas.knuth@gmail.com>
2017-05-02 11:57:39 +0200

    - Tomcat Version changed to 8.0.43 - own version switched to 8.0.43.0 - some Sysout removed

--- src/main/java/org/apache/tomcat/TomcatRunner.java
+++ src/main/java/org/apache/tomcat/TomcatRunner.java
@@ -20,110 +20,108 @@ import java.util.Random;
  */
 public abstract class TomcatRunner implements Runnable {
 
-    protected static int SERVER_LISTENER_PORT;
-    protected static String SERVER_SHUTDOWN_TOKEN = "SHUTDOWN";
-    protected static boolean listenerMode = false;
-    private Random random = null;
-    protected final Configuration configuration;
+	protected static int SERVER_LISTENER_PORT;
 
-    public TomcatRunner(Configuration configuration) {
-        this.configuration=configuration;
-        String stopPort = System.getProperty("stopport");
-        if (stopPort!=null){
-            try {
-                SERVER_LISTENER_PORT = Integer.parseInt(stopPort);
-            } catch (NumberFormatException e){
-                System.err.println("invalid stopport");
-                System.exit(1);
-            }
-        } else if (configuration!=null){
-            SERVER_LISTENER_PORT = configuration.getListenerPort();
-        }
-        System.out.println("Stopport:"+SERVER_LISTENER_PORT);
-        listenerMode=(SERVER_LISTENER_PORT==0)?false:true;
-    }
+	protected static String SERVER_SHUTDOWN_TOKEN = "SHUTDOWN";
 
+	protected static boolean listenerMode = false;
 
-    public void run() {
-        // Set up a server socket to wait on
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket =
-                    new ServerSocket(SERVER_LISTENER_PORT, 1,
-                            InetAddress.getByName("localhost"));
-        } catch (IOException e) {
-            System.err.println("StandardServer.await: create[" + SERVER_LISTENER_PORT
-                    + "]: "+ e.getMessage());
-            System.exit(1);
-        }
+	private Random random = null;
 
-        // Loop waiting for a connection and a valid command
-        while (true) {
+	protected final Configuration configuration;
 
-            // Wait for the next connection
-            Socket socket = null;
-            InputStream stream = null;
-            try {
-                socket = serverSocket.accept();
-                socket.setSoTimeout(10 * 1000);  // Ten seconds
-                stream = socket.getInputStream();
-            } catch (AccessControlException ace) {
-                System.err.println("StandardServer.accept security exception: "
-                        + ace.getMessage() + ace.getMessage());
-                continue;
-            } catch (IOException e) {
-                System.err.println("StandardServer.await: accept: " + e.getMessage());
-                System.exit(1);
-            }
+	public TomcatRunner(Configuration configuration) {
+		this.configuration = configuration;
+		String stopPort = System.getProperty("stopport");
+		if (stopPort != null) {
+			try {
+				SERVER_LISTENER_PORT = Integer.parseInt(stopPort);
+				System.out.println("Stopport:" + SERVER_LISTENER_PORT);
+			} catch (NumberFormatException e) {
+				System.err.println("invalid stopport");
+				System.exit(1);
+			}
+		} else if (configuration != null) {
+			SERVER_LISTENER_PORT = configuration.getListenerPort();
+		}
+		listenerMode = (SERVER_LISTENER_PORT == 0) ? false : true;
+	}
 
-            // Read a set of characters from the socket
-            StringBuffer command = new StringBuffer();
-            int expected = 1024; // Cut off to avoid DoS attack
-            while (expected < SERVER_SHUTDOWN_TOKEN.length()) {
-                if (random == null)
-                    random = new Random();
-                expected += (random.nextInt() % 1024);
-            }
-            while (expected > 0) {
-                int ch = -1;
-                try {
-                    ch = stream.read();
-                } catch (IOException e) {
-                    System.err.println("StandardServer.await: read: " + e.getMessage());
-                    ch = -1;
-                }
-                if (ch < 32)  // Control character or EOF terminates loop
-                    break;
-                command.append((char) ch);
-                expected--;
-            }
+	public void run() {
+		// Set up a server socket to wait on
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = new ServerSocket(SERVER_LISTENER_PORT, 1, InetAddress.getByName("localhost"));
+		} catch (IOException e) {
+			System.err.println("StandardServer.await: create[" + SERVER_LISTENER_PORT + "]: " + e.getMessage());
+			System.exit(1);
+		}
 
-            // Close the socket now that we are done with it
-            try {
-                socket.close();
-            } catch (IOException e) {
-                ;
-            }
+		// Loop waiting for a connection and a valid command
+		while (true) {
 
-            // Match against our command string
-            boolean match = command.toString().equals(SERVER_SHUTDOWN_TOKEN);
-            if (match) {
-                break;
-            } else
-                System.err.println("StandardServer.await: Invalid command '" +
-                        command.toString() + "' received");
+			// Wait for the next connection
+			Socket socket = null;
+			InputStream stream = null;
+			try {
+				socket = serverSocket.accept();
+				socket.setSoTimeout(10 * 1000); // Ten seconds
+				stream = socket.getInputStream();
+			} catch (AccessControlException ace) {
+				System.err.println("StandardServer.accept security exception: " + ace.getMessage() + ace.getMessage());
+				continue;
+			} catch (IOException e) {
+				System.err.println("StandardServer.await: accept: " + e.getMessage());
+				System.exit(1);
+			}
 
-        }
+			// Read a set of characters from the socket
+			StringBuffer command = new StringBuffer();
+			int expected = 1024; // Cut off to avoid DoS attack
+			while (expected < SERVER_SHUTDOWN_TOKEN.length()) {
+				if (random == null)
+					random = new Random();
+				expected += (random.nextInt() % 1024);
+			}
+			while (expected > 0) {
+				int ch = -1;
+				try {
+					ch = stream.read();
+				} catch (IOException e) {
+					System.err.println("StandardServer.await: read: " + e.getMessage());
+					ch = -1;
+				}
+				if (ch < 32) // Control character or EOF terminates loop
+					break;
+				command.append((char) ch);
+				expected--;
+			}
 
-        // Close the server socket and return
-        try {
-            serverSocket.close();
-            stopServer();
-        } catch (IOException e) {
-            ;
-        }
-    }
+			// Close the socket now that we are done with it
+			try {
+				socket.close();
+			} catch (IOException e) {
+				;
+			}
 
-    public abstract void stopServer();
+			// Match against our command string
+			boolean match = command.toString().equals(SERVER_SHUTDOWN_TOKEN);
+			if (match) {
+				break;
+			} else
+				System.err.println("StandardServer.await: Invalid command '" + command.toString() + "' received");
+
+		}
+
+		// Close the server socket and return
+		try {
+			serverSocket.close();
+			stopServer();
+		} catch (IOException e) {
+			;
+		}
+	}
+
+	public abstract void stopServer();
 
 }
--- src/main/java/org/apache/tomcat/TomcatStarter.java
+++ src/main/java/org/apache/tomcat/TomcatStarter.java
@@ -17,160 +17,159 @@ import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.session.StandardManager;
 import org.apache.catalina.startup.Tomcat;
 
-
 /**
  *
  */
 public class TomcatStarter extends TomcatRunner {
-    private Tomcat tomcat;
+	private Tomcat tomcat;
 
-    public TomcatStarter(Configuration configuration) {
-        super(configuration);
-        if (configuration==null){
-            System.err.println("-DtomcatConfig not set");
-            System.exit(1);
-        }
-    }
+	public TomcatStarter(Configuration configuration) {
+		super(configuration);
+		if (configuration == null) {
+			System.err.println("-DtomcatConfig not set");
+			System.exit(1);
+		}
+	}
 
-    @SuppressWarnings("unchecked")
-    private void init() throws Exception {
-        tomcat = new Tomcat();
+	@SuppressWarnings("unchecked")
+	private void init() throws Exception {
+		tomcat = new Tomcat();
 
-        configuration.updateSystemProperties();
+		configuration.updateSystemProperties();
 
-        Iterator<String> contextPathIterator = configuration.getContextPathIterator();
-        while (contextPathIterator.hasNext()) {
-            String contextPath = contextPathIterator.next();
-            createContext(contextPath,configuration.getWebAppSourceDirectory(contextPath));
-        }
-        //tomcat.addWebapp("/frame", new File("webApplication").getAbsolutePath());
+		Iterator<String> contextPathIterator = configuration.getContextPathIterator();
+		while (contextPathIterator.hasNext()) {
+			String contextPath = contextPathIterator.next();
+			createContext(contextPath, configuration.getWebAppSourceDirectory(contextPath));
+		}
+		//tomcat.addWebapp("/frame", new File("webApplication").getAbsolutePath());
 
-        // Erstellen der Konnectoren
-        for (int i = 0; i < configuration.getCountConnectorConfigurations(); i++) {
-            ConnectorConfiguration connConf = configuration.getConnectorConfiguration(i);
-            Connector connector = tomcat.getConnector();
-            connector.setAttribute("address", connConf.getConnectorAdress());
-            connector.setPort(connConf.getConnectorPort());
-            connector.setSecure(connConf.isSecure());
-            connector.setURIEncoding("UTF-8");
+		// Erstellen der Konnectoren
+		for (int i = 0; i < configuration.getCountConnectorConfigurations(); i++) {
+			ConnectorConfiguration connConf = configuration.getConnectorConfiguration(i);
+			Connector connector = tomcat.getConnector();
+			connector.setAttribute("address", connConf.getConnectorAdress());
+			connector.setPort(connConf.getConnectorPort());
+			connector.setSecure(connConf.isSecure());
+			connector.setURIEncoding("UTF-8");
 
-            if (connConf.isSecure()) {
-                if (connConf.getClientAuth() != null) {
-                    connector.setProperty("clientAuth", connConf.getClientAuth());
-                }
-                if (connConf.getKeystoreFile() != null) {
-                    connector.setProperty("keystoreFile", connConf.getKeystoreFile());
-                }
-                if (connConf.getKeystorePass() != null) {
-                    connector.setProperty("keystorePass", connConf.getKeystorePass());
-                }
-                if (connConf.getSslProtocol() != null) {
-                    connector.setProperty("sslProtocol", connConf.getSslProtocol());
-                }
-                if (connConf.getKeyAlias() != null) {
-                    connector.setProperty("keyAlias", connConf.getKeyAlias());
-                }
-                if (connConf.getTruststoreFile() != null) {
-                    connector.setProperty("truststoreFile", connConf.getTruststoreFile());
-                }
-                if (connConf.getTruststorePass() != null) {
-                    connector.setProperty("truststorePass", connConf.getTruststorePass());
-                }
-            }
-        }
+			if (connConf.isSecure()) {
+				if (connConf.getClientAuth() != null) {
+					connector.setProperty("clientAuth", connConf.getClientAuth());
+				}
+				if (connConf.getKeystoreFile() != null) {
+					connector.setProperty("keystoreFile", connConf.getKeystoreFile());
+				}
+				if (connConf.getKeystorePass() != null) {
+					connector.setProperty("keystorePass", connConf.getKeystorePass());
+				}
+				if (connConf.getSslProtocol() != null) {
+					connector.setProperty("sslProtocol", connConf.getSslProtocol());
+				}
+				if (connConf.getKeyAlias() != null) {
+					connector.setProperty("keyAlias", connConf.getKeyAlias());
+				}
+				if (connConf.getTruststoreFile() != null) {
+					connector.setProperty("truststoreFile", connConf.getTruststoreFile());
+				}
+				if (connConf.getTruststorePass() != null) {
+					connector.setProperty("truststorePass", connConf.getTruststorePass());
+				}
+			}
+		}
 
+	}
 
-    }
+	private Context createContext(String path, String docBase) throws Exception {
+		// Erstelle einen Context
+		StandardContext context = (StandardContext) tomcat.addWebapp(path, new File(docBase).getAbsolutePath());
 
-    private Context createContext(String path, String docBase) throws Exception{
-        // Erstelle einen Context
-        StandardContext context = (StandardContext)tomcat.addWebapp(path, new File(docBase).getAbsolutePath());
+		//schaltet die Session Persistenz aus ...
+		StandardManager manager = new StandardManager();
+		manager.setPathname(null);
+		context.setManager(manager);
 
-        //schaltet die Session Persistenz aus ...
-        StandardManager manager =  new StandardManager();
-        manager.setPathname(null);
-        context.setManager(manager);
+		context.setParentClassLoader(this.getClass().getClassLoader());
+		//*** Setzt die classloader Prioritaet auf parent first
+		context.setDelegate(true);
+		//*** Setzt das Flag, mit dem bei Aenderung ein Neustart angefordert wird
+		//context.setReloadable(true);
 
-        context.setParentClassLoader(this.getClass().getClassLoader());
-        //*** Setzt die classloader Prioritaet auf parent first
-        context.setDelegate(true);
-        //*** Setzt das Flag, mit dem bei Aenderung ein Neustart angefordert wird
-        //context.setReloadable(true);
+		// Erstellen eines handlers fuer jsps
+		Wrapper jspServlet = context.createWrapper();
+		jspServlet.setName("jsp");
+		jspServlet.setServletClass("org.apache.jasper.servlet.JspServlet");
+		jspServlet.addInitParameter("fork", "false");
+		jspServlet.addInitParameter("xpoweredBy", "false");
+		jspServlet.addInitParameter("enablePooling", "false");
+		jspServlet.addInitParameter("compilerSourceVM", "1.8");
+		jspServlet.addInitParameter("compilerTargetVM", "1.8");
+		jspServlet.addInitParameter("javaEncoding", "UTF8");
+		jspServlet.setLoadOnStartup(2);
+		context.addChild(jspServlet);
+		context.addServletMapping("*.jsp", "jsp");
+		context.addServletMapping("*.jspx", "jsp");
 
-        // Erstellen eines handlers fuer jsps
-        Wrapper jspServlet = context.createWrapper();
-        jspServlet.setName("jsp");
-        jspServlet.setServletClass("org.apache.jasper.servlet.JspServlet");
-        jspServlet.addInitParameter("fork", "false");
-        jspServlet.addInitParameter("xpoweredBy", "false");
-        jspServlet.addInitParameter("enablePooling", "false");
-        jspServlet.addInitParameter("compilerSourceVM", "1.6");
-        jspServlet.addInitParameter("compilerTargetVM", "1.6");
-        jspServlet.addInitParameter("javaEncoding", "UTF8");
-        jspServlet.setLoadOnStartup(2);
-        context.addChild(jspServlet);
-        context.addServletMapping("*.jsp", "jsp");
-        context.addServletMapping("*.jspx", "jsp");
+		//welcome files
+		context.addWelcomeFile("indexs");
+		context.setSessionTimeout(30);
 
-        //welcome files
-        context.addWelcomeFile("indexs");
-        context.setSessionTimeout(30);
+		//Einige mime mappings
+		context.addMimeMapping("html", "text/html");
+		context.addMimeMapping("htm", "text/html");
+		context.addMimeMapping("gif", "image/gif");
+		context.addMimeMapping("jpg", "image/jpeg");
+		context.addMimeMapping("png", "image/png");
+		context.addMimeMapping("js", "text/javascript");
+		context.addMimeMapping("css", "text/css");
+		context.addMimeMapping("pdf", "application/pdf");
 
-        //Einige mime mappings
-        context.addMimeMapping("html", "text/html");
-        context.addMimeMapping("htm", "text/html");
-        context.addMimeMapping("gif", "image/gif");
-        context.addMimeMapping("jpg", "image/jpeg");
-        context.addMimeMapping("png", "image/png");
-        context.addMimeMapping("js", "text/javascript");
-        context.addMimeMapping("css", "text/css");
-        context.addMimeMapping("pdf", "application/pdf");
+		return context;
+	}
 
-        return context;
-    }
+	public void startServer() throws Exception {
+		init();
+		tomcat.start();
+	}
 
-    public void startServer() throws Exception {
-        init();
-        tomcat.start();
-    }
+	@Override
+	public void stopServer() {
+		if (tomcat != null) {
+			try {
+				System.out.println("Shutting down MyServer...");
+				tomcat.stop();
+				System.out.println("MyServer shutdown.");
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
 
-    public void stopServer() {
-        if (tomcat != null) {
-            try {
-                System.out.println("Shutting down MyServer...");
-                tomcat.stop();
-                System.out.println("MyServer shutdown.");
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
+	public static void main(String args[]) throws Exception {
+		Configuration configuration = null;
+		String config = System.getProperty("tomcatConfig");
+		if (config == null) {
+			System.err.println("-DtomcatConfig not set");
+			String basedir = System.getProperty("basedir") == null ? "webApplication" : System.getProperty("basedir");
+			configuration = new Configuration(basedir);
+			String contextPath = System.getProperty("contextPath") == null ? "/" : System.getProperty("contextPath");
+			configuration.addWebApp(contextPath, basedir);
+			int port = System.getProperty("port") == null ? 8080 : Integer.parseInt(System.getProperty("port"));
+			configuration.setListenerPort(port);
+		} else {
+			configuration = ConfigurationReader.readConfiguration(config);
+		}
 
-    public static void main(String args[]) throws Exception {
-        Configuration configuration = null;
-        String config = System.getProperty("tomcatConfig");
-        if (config == null) {
-            System.err.println("-DtomcatConfig not set");
-            String basedir = System.getProperty("basedir")==null?"webApplication":System.getProperty("basedir");
-            configuration = new Configuration(basedir);
-            String contextPath = System.getProperty("contextPath")==null?"/":System.getProperty("contextPath");
-            configuration.addWebApp(contextPath, basedir);
-            int port = System.getProperty("port")==null?8080:Integer.parseInt(System.getProperty("port"));
-            configuration.setListenerPort(port);
-        } else {
-            configuration = ConfigurationReader.readConfiguration(config);
-        }
-
-        TomcatStarter server = new TomcatStarter(configuration);
-        server.startServer();
-        Thread thread = new Thread(server);
-        if (listenerMode){
-            thread.start();
-            System.out.println("Stop Port activated --> "+SERVER_LISTENER_PORT);
-        } else {
-            server.tomcat.getServer().await();
-        }
-        //
-    }
+		TomcatStarter server = new TomcatStarter(configuration);
+		server.startServer();
+		Thread thread = new Thread(server);
+		if (listenerMode) {
+			thread.start();
+			System.out.println("Stop Port activated --> " + SERVER_LISTENER_PORT);
+		} else {
+			server.tomcat.getServer().await();
+		}
+		//
+	}
 
 }
